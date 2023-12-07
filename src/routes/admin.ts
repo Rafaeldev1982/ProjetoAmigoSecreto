5d39e000e6591ba0d9de440b6046b5d9e783adef
Rafaeldev1982 <eurafaelpereira@gmail.com>
2023-12-07 20:37:34 -0300

    Grupos: Listando todos

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import * as auth from '../controllers/auth';
 import * as events from '../controllers/events';
-
+import * as groups from '../controllers/groups';
 const router = Router();
 
 router.post('/login', auth.login);
@@ -12,8 +12,9 @@ router.get('/events', auth.validate, events.getAll);
 router.get('/events/:id', auth.validate, events.getEvent);
 router.post('/envents', auth.validate, events.addEvent);
 router.delete('/events/:id', auth.validate, events.deleteEvent)
-
-
 router.put('/events/:id', auth.validate, events.updateEvent);
+
+
+router.get('/events/:id_event/groups', auth.validate, groups.getAll)
 
 export default router;
